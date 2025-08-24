1a37f25f32d2af46f151734e1e1f742e1a5123b3
dhruvth16 <dhruvthakur1616@gmail.com>
2025-08-24 16:13:18 +0530

    fix: cors

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://ai-course-visualizer.vercel.app/", "https://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
